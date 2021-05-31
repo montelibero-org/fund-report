9dc6083723836c95bd63c5e714bbe237eb31ae17
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-05-31 18:30:13 +0300

    Add

---
+++ Main.hs
@@ -0,0 +1,116 @@
+module Main (main) where
+
+import           Control.Lens                           ((&~), (.=), (.~), (^.))
+import           Control.Monad                          (void)
+import           Data.Aeson                             (FromJSON)
+import           Data.Default                           (def)
+import           Data.Function                          ((&))
+import           GHC.Generics                           (Generic)
+import           Graphics.Rendering.Chart               (HTextAnchor (HTA_Centre),
+                                                         PickFn, PieLayout (..),
+                                                         Renderable,
+                                                         VTextAnchor (VTA_Top),
+                                                         addMargins,
+                                                         fillBackground, label,
+                                                         nullPickFn,
+                                                         pieChartToRenderable,
+                                                         pie_data, pie_plot,
+                                                         pie_title, pitem_label,
+                                                         pitem_value, setPickFn)
+import           Graphics.Rendering.Chart.Backend.Cairo (fo_size,
+                                                         renderableToFile)
+import           Graphics.Rendering.Chart.Easy          (execEC)
+import           Graphics.Rendering.Chart.Grid          (Grid, aboveN,
+                                                         gridToRenderable, tval,
+                                                         weights)
+import           Network.Wreq                           (asJSON, get,
+                                                         responseBody)
+
+newtype ResponseOk a = ResponseOk{_embedded :: Embedded a}
+  deriving (FromJSON, Generic)
+
+newtype Embedded a = Embedded{records :: [a]}
+  deriving (FromJSON, Generic)
+
+data Holder = Holder{account, balance :: String}
+  deriving (FromJSON, Generic)
+
+data Foundation = Foundation
+  {assetName, assetIssuer :: String, treasury :: Maybe String}
+
+main :: IO ()
+main =
+  do
+    mtlHolders     <- getHolders mtl
+    mtlcityHolders <- getHolders mtlcity
+    let
+      grid =
+        aboveN
+          [ pieToGrid $ holdersPie mtl     mtlHolders
+          , pieToGrid $ holdersPie mtlcity mtlcityHolders
+          ]
+    void $
+      renderableToFile fileOptions reportFile $
+      fillBackground def $
+      gridToRenderable grid
+  where
+    mtl =
+      Foundation
+        { assetName = "MTL"
+        , assetIssuer =
+            "GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V"
+        , treasury =
+            Just "GDX23CPGMQ4LN55VGEDVFZPAJMAUEHSHAMJ2GMCU2ZSHN5QF4TMZYPIS"
+        }
+    mtlcity =
+      Foundation
+        { assetName = "MTLCITY"
+        , assetIssuer =
+            "GDUI7JVKWZV4KJVY4EJYBXMGXC2J3ZC67Z6O5QFP4ZMVQM2U5JXK2OK3"
+        , treasury = Nothing
+        }
+    reportFile  = "report.png"
+    fileOptions = def & fo_size .~ (1500, 1000)
+
+assetId :: Foundation -> String
+assetId Foundation{assetName, assetIssuer} = assetName <> "-" <> assetIssuer
+
+holdersPie :: Foundation -> [Holder] -> PieLayout
+holdersPie Foundation{assetName, treasury} holders =
+  execEC $ do
+    pie_title .= assetName <> " asset holders"
+    pie_plot . pie_data .=
+        [ pitem (read balance) account
+        | Holder{account, balance} <- holders
+        , Just account /= treasury
+        ]
+  where
+    pitem value lbl = def &~ do
+      pitem_value .= value
+      pitem_label .= lbl
+
+pieToGrid :: PieLayout -> Grid (Renderable (PickFn a))
+pieToGrid PieLayout{_pie_margin, _pie_plot, _pie_title, _pie_title_style} =
+  aboveN
+    [ tval $ addMargins (_pie_margin / 2, 0, 0, 0) (setPickFn nullPickFn title)
+    , weights (1, 1) $ tval $
+      addMargins (_pie_margin, _pie_margin, _pie_margin, _pie_margin) $
+      pieChartToRenderable _pie_plot
+    ]
+  where
+    title = label _pie_title_style HTA_Centre VTA_Top _pie_title
+
+getHolders :: Foundation -> IO [Holder]
+getHolders asset =
+  do
+    r <-
+      asJSON =<<
+      get
+        (concat
+          [ "https://api.stellar.expert/explorer/", network
+          , "/asset/", assetId asset, "/holders"
+          ])
+    let ResponseOk{_embedded = Embedded{records}} = r ^. responseBody
+    pure records
+  where
+    network = "public"
