13bfc82d654ca2f26aba8db7cf3d945ebec2e842
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-05-31 19:47:14 +0300

    Add member substitution

--- Main.hs
+++ Main.hs
@@ -5,6 +5,9 @@ import           Control.Monad (void)
 import           Data.Aeson (FromJSON)
 import           Data.Default (def)
 import           Data.Function ((&))
+import           Data.List (sortOn)
+import qualified Data.Map.Strict as Map
+import           Data.Ord (Down (..))
 import           Data.Ratio ((%))
 import           GHC.Generics (Generic)
 import           Graphics.Rendering.Chart (HTextAnchor (HTA_Centre), PickFn,
@@ -32,19 +35,23 @@ newtype Embedded a = Embedded{records :: [a]}
 data Holder = Holder{account, balance :: String}
   deriving (FromJSON, Generic)
 
+data Member = Member{account :: String, balance :: Rational}
+
 data Foundation = Foundation
   {assetName, assetIssuer :: String, treasury :: Maybe String}
 
 main :: IO ()
 main =
   do
-    mtlHolders     <- getHolders mtl
-    mtlcityHolders <- getHolders mtlcity
+    mtlMembers     <- getMembers mtl
+    mtlcityMembers <- getMembers mtlcity
+    let mtlcityViaMtlMembers = substMembers mtl mtlMembers mtlcityMembers
     let
       grid =
         aboveN
-          [ pieToGrid $ holdersPie mtl     mtlHolders
-          , pieToGrid $ holdersPie mtlcity mtlcityHolders
+          [ pieToGrid $ membersPie mtl     mtlMembers
+          , pieToGrid $ membersPie mtlcity mtlcityMembers
+          , pieToGrid $ membersPie mtlcity mtlcityViaMtlMembers
           ]
     void $
       renderableToFile fileOptions reportFile $
@@ -67,35 +74,28 @@ main =
         , treasury = Nothing
         }
     reportFile  = "report.png"
-    fileOptions = def & fo_size .~ (1500, 1000)
+    fileOptions = def & fo_size .~ (1000, 1500)
 
 assetId :: Foundation -> String
 assetId Foundation{assetName, assetIssuer} = assetName <> "-" <> assetIssuer
 
-holdersPie :: Foundation -> [Holder] -> PieLayout
-holdersPie Foundation{assetName, treasury} holders =
+membersPie :: Foundation -> [Member] -> PieLayout
+membersPie Foundation{assetName} members =
   execEC $ do
     pie_title .= assetName <> " holders/members"
     pie_plot . pie_label_style . font_size .= 20
     pie_plot . pie_data .=
       [ def &~ do
-          pitem_value .= balanceFloat
+          pitem_value .= realToFrac balance
           pitem_label .=
             "..." <> drop (length account - 4) account <> ", " <>
-            showLocal ' ' (round (balanceInt % 10_000_000) :: Integer) <>
-            ", " <> printf "%.1f" share <> "%"
-      | (account, balanceInt) <- members
-      , let
-        balanceFloat = fromIntegral balanceInt
-        share = balanceFloat / fromIntegral sumBalance * 100
+            showLocal ' ' (round balance :: Integer) <>
+            ", " <> printf "%.1f%%" (realToFrac share :: Double)
+      | Member{account, balance} <- members
+      , let share = balance / sumBalance * 100
       ]
   where
-    members =
-      [ (account, read @Integer balance)
-      | Holder{account, balance} <- holders
-      , Just account /= treasury
-      ]
-    sumBalance = sum $ map snd members
+    sumBalance = sum $ map (\Member{..} -> balance) members
 
 pieToGrid :: PieLayout -> Grid (Renderable (PickFn a))
 pieToGrid PieLayout{_pie_margin, _pie_plot, _pie_title, _pie_title_style} =
@@ -108,15 +108,25 @@ pieToGrid PieLayout{_pie_margin, _pie_plot, _pie_title, _pie_title_style} =
   where
     title = label _pie_title_style HTA_Centre VTA_Top _pie_title
 
+getMembers :: Foundation -> IO [Member]
+getMembers foundation@Foundation{treasury} = do
+  holders <- getHolders foundation
+  pure
+    [ Member{account, balance = read @Integer balance % 10_000_000}
+    | Holder{account, balance} <- holders
+    , Just account /= treasury
+    ]
+
+
 getHolders :: Foundation -> IO [Holder]
-getHolders asset =
+getHolders foundation =
   do
     r <-
       asJSON =<<
       get
         (concat
           [ "https://api.stellar.expert/explorer/", network
-          , "/asset/", assetId asset, "/holders"
+          , "/asset/", assetId foundation, "/holders"
           ])
     let ResponseOk{_embedded = Embedded{records}} = r ^. responseBody
     pure records
@@ -127,3 +137,23 @@ showLocal :: Char -> Integer -> String
 showLocal groupSeparator = snd . foldr go (0 :: Int, "") . show where
   go c (len, r) =
     (len + 1, c : [groupSeparator | len `mod` 3 == 0, len > 0] ++ r)
+
+substMembers :: Foundation -> [Member] -> [Member] -> [Member]
+substMembers Foundation{treasury = parentTreasury} parentMembers members =
+  map (\(account, balance) -> Member{..}) $
+    sortOn (Down . snd) $
+    Map.assocs $
+    Map.fromListWith (+) $
+    concat
+      [ if Just directAccount == parentTreasury then let
+          parentBalance = directBalance
+          in
+          [ (account, balance / sumParent * parentBalance)
+          | Member{account, balance} <- parentMembers
+          ]
+        else
+          [(directAccount, directBalance)]
+      | Member{account = directAccount, balance = directBalance} <- members
+      ]
+  where
+    sumParent = sum $ map (\Member{..} -> balance) parentMembers
